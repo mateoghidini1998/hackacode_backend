eee7d28e8b145f98765225b22528919f5844d48a
mateoghidini1998 <mateghidini123@gmail.com>
2023-06-01 10:59:16 -0300

    created server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const http = require('http');
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
 
-const server = http.createServer((req, res) => {
+//Load env variables
 
-});
+dotenv.config({ path:'./config/config.env'})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
