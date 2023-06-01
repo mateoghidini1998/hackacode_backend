bf0324bf5499a07d7ae19c4d51d15190d799da2a
mateoghidini1998 <mateghidini123@gmail.com>
2023-06-01 15:11:36 -0300

    created middlewares for async methods and error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 const User = require('../models/User');
 const Employee = require('../models/Employee');
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc   Register user
 // @route  POST /api/auth/register
 // @access Public
 
-exports.register = async (req, res, next) => {
-    try {
+exports.register = asyncHandler(async (req, res, next) => {
         const { email, password, name, lastName } = req.body;
         const user = await User.create({
             email,
@@ -23,73 +24,52 @@ exports.register = async (req, res, next) => {
             success: true,
             data: user, employee
         })
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({
-            success: false
-        });
-    }
-};
+    
+});
 
 // @desc   Get users
 // @route  GET /api/auth/users
 // @access Public
 
-exports.getUsers = async (req, res, next) => {
-    try {
+exports.getUsers = asyncHandler(async (req, res, next) => {
         const users = await User.find();
         res.status(200).json({
             success: true,
             count: users.length,
             data: users
         })
-    } catch (error) {
-        res.status(400).json({
-            success: false
-        })
-    }
-};
+
+    
+});
 
 // @desc   Get a user
 // @route  GET /api/auth/users/:id
 // @access Public
 
-exports.getUser = async (req, res, next) => {
-    try {
+exports.getUser = asyncHandler(async (req, res, next) => {
         const user = await User.findById(req.params.id);;
         if(!user){
-            res.status(400).json({
-                success: false
-            });
+            return next (new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({
             success: true,
             data: user
         })
-
-    } catch (error) {
-        res.status(400).json({
-            success: false
-        })
-    }
-};
+});
 
 // @desc   Update a user
 // @route  PUT /api/auth/users/:id
 // @access Private
 
-exports.updateUser = async (req, res, next) => {
-    try {
+exports.updateUser = asyncHandler(async (req, res, next) => {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
         })
 
         if(!user){
-            res.status(400).json({
-                success: false
-            });
+            return next (new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({
@@ -97,25 +77,17 @@ exports.updateUser = async (req, res, next) => {
             data: user
         })
 
-    } catch (error) {
-        res.status(400).json({
-            success: false
-        })
-    }
-};
+});
 
 
 // @desc   Delete a user
 // @route  DELETE /api/auth/users/:id
 // @access Private
 
-exports.deleteUser = async (req, res, next) => {
-    try {
+exports.deleteUser = asyncHandler(async (req, res, next) => {
         const user = await User.findByIdAndDelete(req.params.id);
         if(!user){
-            res.status(400).json({
-                success: false
-            });
+            return next (new ErrorResponse(`User not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({
@@ -123,20 +95,5 @@ exports.deleteUser = async (req, res, next) => {
             data: {}
         })
 
-    } catch (error) {
-        res.status(400).json({
-            success: false
-        })
-    }
-};
+});
 
-/* exports.loginUser = async (req, res, next) => {
-
-    const { email, password } = req.body;
-
-    if(!email || !password){
-        return next(new ErrorResponse('Please provide an email and password', 400));
-    }
-
-
-} */
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error');
 
 const app = express();
 
@@ -22,6 +23,8 @@ dotenv.config({ path:'./config/config.env'});
 connectDB();
 
 app.use('/api/auth',  auth);
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
