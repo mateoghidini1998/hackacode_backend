d6072b0668dcf38eaeb661c07300d017ecc95b98
mateoghidini1998 <mateghidini123@gmail.com>
2023-06-06 15:44:48 -0300

    fixed seeders

--- seeder.js
+++ seeder.js
@@ -3,53 +3,52 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 const dotenv = require('dotenv');
 
-//Load env vars
+//Load env variables
+
 dotenv.config({ path: './config/config.env' });
 
 //Load models
 const User = require('./models/User');
 const Employee = require('./models/Employee');
 
+
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true
 });
- 
+
 //Read JSON files
-
-const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
 const employees = JSON.parse(fs.readFileSync(`${__dirname}/_data/employees.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
 
-//Import data into DB
-
+//Import into DB
 const importData = async () => {
-    try {
+    try{
         await User.create(users);
         await Employee.create(employees);
-
-        console.log('Data imported...'.green.inverse);
-        process.exit(); 
-    } catch (error) {
-        console.error(error);
-    }
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
     
+    } catch (err) {
+        console.error(err);
+    }
 }
 
+//Delete data
 const deleteData = async () => {
-    try {
-        await User.deleteMany(users);
-        await Employee.deleteMany(employees);
-
-        console.log('Data deleted...'.red.inverse);
-        process.exit(); 
-    } catch (error) {
-        console.error(error);
-    }
+    try{
+        await User.deleteMany();
+        await Employee.deleteMany();
+        console.log('Data destroyed...'.red.inverse);
+        process.exit();
     
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 if(process.argv[2] === '-i'){
     importData();
-} else if(process.argv[2] === '-d'){
-    deleteData();l
+} else if (process.argv[2] === '-d') {
+    deleteData();
 }
